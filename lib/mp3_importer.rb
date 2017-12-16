e9a2ab2c9cc4aa46079457d18a8543c00ff6b63c
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-16 20:09:31 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,12 +4,14 @@ class MP3Importer
   def initialize(path)
     @path = path
   end
-
-  def files
-    @files ||=Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+ 
+ def files
+     @files ||=Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
-
-  def import
-    files.each {|f| Song.new_by_filename(f)}
+ 
+   def import
+     files.each {|f| Song.new_by_filename(f)}
   end
-end
+ 
+ end
+   
