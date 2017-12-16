101def82013d802b99f530c478bcd92308593cc2
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-16 19:51:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,41 +1,39 @@
-class Artist 
-  attr_accessor :name, :songs 
+class Artist
+  attr_accessor :name, :songs
 
   @@all = []
 
   def initialize(name)
-    @name = name 
+    @name = name
     @songs = []
-  end 
+  end
 
   def self.all
-    @@all 
-  end 
+    @@all
+  end
 
   def add_song(song)
-    @songs << song 
-  end 
+    @songs << song
+  end
 
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.create(name)
-  end 
+  end
 
   def self.find(name)
     self.all.find {|artist| artist.name == name}
-  end 
+  end
 
   def self.create(name)
     self.new(name).tap {|artist| artist.save}
-  end 
+  end
 
-  def save 
-    @@all << self 
-  end 
+  def save
+    @@all << self
+  end
 
-  def print_songs 
+  def print_songs
     songs.each {|song| puts song.name}
-  end 
+  end
 
-end 
-
-    
+end
